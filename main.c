c3c9d47675c18fc9a5518367d4a5b314531a5146
Mariam Eid <meid@lab3r7s14.42abudhabi.ae>
2025-02-21 19:06:44 +0400

    lol

--- main.c
+++ main.c
@@ -6,11 +6,20 @@
 /*   By: meid <meid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:02:01 by meid              #+#    #+#             */
-/*   Updated: 2025/02/15 07:39:22 by meid             ###   ########.fr       */
+/*   Updated: 2025/02/18 12:12:09 by meid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void *philofunction(void *arg)
+{
+    int i = *(int*)arg;
+    printf("\033[33m philo :%d is: \033[0m", i);
+    print_emoji("eat");
+    printf("\n");
+    return (NULL);
+} 
 
 t_info fill_some_info(int ac, char **av)
 {
@@ -27,9 +36,42 @@ t_info fill_some_info(int ac, char **av)
     return (i);
     
 }
-void philo(t_info *info)
+
+int philo(t_info *info)
 {
-    (void)info;
+    pthread_t *th = malloc(sizeof(pthread_t) * info->number_of_philosophers);
+    if (!th)
+        return (1);
+    int i = 0;
+    int flag = 0; 
+    while(i < info->number_of_philosophers)
+    {
+        int *index = malloc(sizeof(int));
+        if (!index)
+        {
+            perror("malloc failed");
+            return 1;
+        }
+        *index = i;
+        if (flag == 0)
+        {
+            pthread_create(&th[i], NULL, &philofunction, index);
+            flag = 1;
+        }
+        else if (flag == 1)
+        {
+            pthread_create(&th[i], NULL, &philofunction, index);
+            flag = 0;
+        }
+        i++;
+    }
+    i = 0;
+    while(i < info->number_of_philosophers)
+    {
+        pthread_join(th[i], NULL);// the return value of a function we can resive it here
+        i++;
+    }
+    return (0);
 }
 
 
@@ -48,6 +90,6 @@ int main(int ac, char **av)
     print_emoji("eat");
     print_emoji("sleep");
     print_emoji("die");
-    philo(&info);
-    
+    if (philo(&info))
+        return (print_with_color("error in philo", "red"), 1);
 }
